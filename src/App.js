183b60e0dd135201f1b25ebbd26af8404f384de8
chamudithaperera <chamudithakavishan2001@gmail.com>
2024-11-26 14:48:40 +0530

    update login and app.js

--- src/App.js
+++ src/App.js
@@ -2,9 +2,9 @@ import './App.css';
 import {BrowserRouter as Router, Routes, Route, Link, NavLink} from 'react-router-dom';
 import Login from './components/Login';
 import Dashboard from './components/Dashboard';
-// import Home from './components/Home';
-// import AboutUs from './components/Aboutus';
-// import Contactus from './components/Contactus';
+import Home from './components/Home';
+import AboutUs from './components/Aboutus';
+import Contactus from './components/Contactus';
 
 function App() {
   return (
@@ -18,14 +18,15 @@ function App() {
         </ul>
       </nav> */}
         <Routes>
-          <Route path='/' elements={Login}/>
-          <Route path='/dash' elements={Dashboard}/>
+          <Route path='/' elements={<Login/>}/>
+          <Route path='/dash' elements={<Dashboard/>}/>
           {/* <Route path='/home' element={<Home/>}/>
           <Route path='/about' element={<AboutUs/>}/>
           <Route path='/contact' element={<Contactus/>}/> */}
 
         </Routes>
       </Router>
+      
     </div>
   );
 }
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,9 +1,16 @@
-export default function Login()
-{
-    return(
+import { useNavigate } from "react-router-dom";
+
+export default function Login() {
+    const navigate = useNavigate();
+    
+    const aunthenticates = () => {
+        navigate('/dash');
+    }
+
+    return (
         <div>
             <h1>Login here</h1>
-            <button>Login</button>
+            <button onClick={aunthenticates}>Login</button>
         </div>
     );
 }
