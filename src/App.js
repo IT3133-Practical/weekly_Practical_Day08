1a6d1f658f755cf38d7e7d5c13f55fd8f540863d
chamudithaperera <chamudithakavishan2001@gmail.com>
2024-11-26 14:32:36 +0530

    connect login and dashboard to app.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
 import './App.css';
 import {BrowserRouter as Router, Routes, Route, Link, NavLink} from 'react-router-dom';
-import Home from './components/Home';
-import AboutUs from './components/Aboutus';
-import Contactus from './components/Contactus';
+import Login from './components/Login';
+import Dashboard from './components/Dashboard';
+// import Home from './components/Home';
+// import AboutUs from './components/Aboutus';
+// import Contactus from './components/Contactus';
 
 function App() {
   return (
     <div className="App">
       <Router>
-      <nav>
+      {/* <nav>
         <ul>
           <li><Link to="/home">Home</Link></li>
           <li><Link to="/about">About</Link></li>
           <li><Link to="/contact">Contact</Link></li>
         </ul>
-      </nav>
+      </nav> */}
         <Routes>
-          <Route path='/home' element={<Home/>}/>
+          <Route path='/' elements={Login}/>
+          <Route path='/dash' elements={Dashboard}/>
+          {/* <Route path='/home' element={<Home/>}/>
           <Route path='/about' element={<AboutUs/>}/>
-          <Route path='/contact' element={<Contactus/>}/>
+          <Route path='/contact' element={<Contactus/>}/> */}
 
         </Routes>
       </Router>
