8314f94b48323961e6f142644c9986ccd201640f
chamudithaperera <chamudithakavishan2001@gmail.com>
2024-11-26 14:13:14 +0530

    create navbar

--- src/App.js
+++ src/App.js
@@ -1,15 +1,26 @@
 import './App.css';
+import {BrowserRouter as Router, Routes, Route, Link, NavLink} from 'react-router-dom';
+import Home from './components/Home';
+import AboutUs from './components/Aboutus';
+import Contactus from './components/Contactus';
 
 function App() {
   return (
     <div className="App">
       <Router>
-        <Routers>
+      <nav>
+        <ul>
+          <li><Link to="/home">Home</Link></li>
+          <li><Link to="/about">About</Link></li>
+          <li><Link to="/contact">Contact</Link></li>
+        </ul>
+      </nav>
+        <Routes>
           <Route path='/home' element={<Home/>}/>
           <Route path='/about' element={<AboutUs/>}/>
           <Route path='/contact' element={<Contactus/>}/>
 
-        </Routers>
+        </Routes>
       </Router>
     </div>
   );
